1078697fdecf80c735a5d9258f95e18d400e1399
Rhys-Wiers <rhysmsu@gmail.com>
2026-04-14 16:48:43 -0400

    some improvements in next_index function and adds list of degrees for each vertex

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 # TODO: convert to a list of edges instead of adjacency matrix?
 import numpy as np
-
+import copy
 
 class State:
     def __init__(self, n):
        self.matrix = np.zeros((n, n))
        self.curr_size: int = 0
-       self.included: int = 0
+       # self.included: int = 0
        self.excluded: int = 0
+       self.degrees = np.empty(n)
        #self.mask: int = 0 # for included/excluded
        self.completed_mask = np.zeros((n, n))
 
@@ -15,80 +16,90 @@ class State:
 def solver(state, best_size):
 
     # TODO: Add a bound here that compares current and best_size so below works
+
     idx = next_index(state)
     if idx == -1:
-        return
+        return best_size
 
-    exclude_state = state
+    exclude_state = copy.deepcopy(state)
 
     # include -> state(updated)
+    update_degrees(state, idx)
     state.matrix[idx, :] = 0
     state.matrix[:, idx] = 0
-    state.included |= (1 << idx)
+    # state.included |= (1 << idx)
     state.curr_size += 1 
-    solver(state, best_size)
+    in_sol = solver(state, best_size)
 
-    best_size = min(best_size, state.curr_size)
+    best_size = min(best_size, in_sol)
     
     # exclude -> state
     state.excluded |= (1 << idx)
-    solver(exclude_state, best_size)
+    ex_sol = solver(exclude_state, best_size)
 
-    best_size = min(best_size, exclude_state.curr_size)
-    return
+    best_size = min(best_size, ex_sol)
+    return best_size
 
-def next_index(state) -> int:
-    max_vals = 0
-    index = -1
-    for idx in range(state.matrix.shape[0]):
-        if (state.included & (1 << idx) == 0) and (state.excluded & (1 << idx) == 0):
-            vals = np.count_nonzero(matrix[idx])
-            if vals > max_vals:
-                max_vals = vals
-                index = idx
+def next_index(state: State) -> int:
+    # TODO: make it so this function doesnt grab vertices that have 
+    #       already been considered
+    max_deg_idx = int(np.argmax(state.degrees))
+    max_deg = int(np.max(state.degrees))
+    return max_deg_idx if max_deg > 0 else -1
 
-    return index
+def update_degrees(state: State, index: int):
+    # Grab row of adjacency matrix and find indices that need to be updated
+    row = state.matrix[index]
+    indices = np.nonzero(row)[0]
+
+    # Update degrees accordingly
+    state.degrees[indices] -= 1
+    state.degrees[index] = 0
 
 def approximate(state: State) -> int:
     while np.any(state.matrix):
-
         idx = next_index(state)
         if idx == -1:
             return state.curr_size
 
+        # Keep track of how many uncovered edges touch each vertex
+        update_degrees(state, idx)
         state.matrix[idx, :] = 0
         state.matrix[:, idx] = 0
 
         state.curr_size += 1
 
         #NOTE: Don't need this, but here for now
-        state.included |= (1 << idx)
+        # state.included |= (1 << idx)
 
     return state.curr_size
-
-
     # repeat until valid solution
-
-
 
 if __name__ == "__main__":
 
     n, m = list(map(int, input().split()))
-    
     matrix = np.zeros((n, n))
+    degrees = np.zeros(n)
 
     # TODO: Use triangular later
-    for _ in range(m):
+    for i in range(m):
         row, col = list(map(int, input().split()))
         matrix[row, col] = 1
         matrix[col, row] = 1
-    
+        degrees[row] += 1
+        degrees[col] += 1
+
+
     # Create initial state
     state = State(n)
     state.matrix = matrix
-
+    state.degrees = degrees
+    
+    # Get approximate solution
+    approx_state = copy.deepcopy(state)
+    best_guess = approximate(approx_state)
+    
     # Get solution
-    best_guess = approximate(state)
-    solver(state, best_guess)
+    solution = solver(state, best_guess)
 
-    print(state.curr_size)
+    print(solution)
